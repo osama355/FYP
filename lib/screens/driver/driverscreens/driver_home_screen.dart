6a5c80b2d66e97d064423c7ff0fc852a8ecaf01e
osama355 <muhammadosama3556@gmail.com>
2022-11-22 11:47:02 +0500

    add search place api

--- lib/screens/driver/driverscreens/driver_home_screen.dart
+++ lib/screens/driver/driverscreens/driver_home_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import 'package:drive_sharing_app/screens/driver/driverscreens/driver_sidebar.dart';
+import 'package:drive_sharing_app/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DriverPost extends StatefulWidget {
   const DriverPost({super.key});
@@ -9,16 +13,60 @@ class DriverPost extends StatefulWidget {
 }
 
 class _DriverPost extends State<DriverPost> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  final CameraPosition _kGooglePlex =
+      const CameraPosition(target: LatLng(24.839108, 67.186298), zoom: 14);
+
+  final List<Marker> _markers = [];
+
+  Future<Position> getCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      Utils().toastMessage(error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+
+  loadData() {
+    getCurrentLocation().then((value) async {
+      print("My current location");
+      print("${value.latitude} ${value.longitude}");
+      _markers.add(Marker(
+          markerId: MarkerId("2"),
+          position: LatLng(value.latitude, value.longitude),
+          infoWindow: InfoWindow(title: "Current Location")));
+
+      CameraPosition cameraPosition = CameraPosition(
+          zoom: 14, target: LatLng(value.latitude, value.longitude));
+
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const DriverSidebar(),
       appBar: AppBar(
-        backgroundColor: const Color(0xff4BA0FE),
-        centerTitle: false,
         title: const Text("Home"),
       ),
-      body: const Center(child: Text("Home")),
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(_markers),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
--- lib/screens/driver/driverscreens/googlemap/offer_ride.dart
+++ lib/screens/driver/driverscreens/googlemap/offer_ride.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:drive_sharing_app/screens/driver/driverscreens/driver_sidebar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -12,13 +11,14 @@ class OfferRide extends StatefulWidget {
 }
 
 class _OfferRideState extends State<OfferRide> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex =
+  final CameraPosition _kGooglePlex =
       const CameraPosition(target: LatLng(24.839108, 67.186298), zoom: 14.4749);
 
-  List<Marker> _marker = [];
-  List<Marker> _list = const [
+  final List<Marker> _marker = [];
+
+  final List<Marker> _list = const [
     Marker(
         markerId: MarkerId("1"),
         position: LatLng(24.839108, 67.186298),
@@ -42,11 +42,24 @@ class _OfferRideState extends State<OfferRide> {
       appBar: AppBar(
         title: const Text("Offer Ride"),
       ),
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        markers: Set<Marker>.of(_marker),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.location_disabled_outlined),
+        onPressed: () async {
+          GoogleMapController controller = await _controller.future;
+          controller.animateCamera(CameraUpdate.newCameraPosition(
+              const CameraPosition(
+                  target: LatLng(24.865767427714033, 67.07451124295166),
+                  zoom: 14)));
+          setState(() {});
         },
       ),
     );
