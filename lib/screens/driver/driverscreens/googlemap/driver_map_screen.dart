56771d0ecbc2666ed3dee2fc2158084fd631cb13
osama355 <muhammadosama3556@gmail.com>
2022-12-06 07:06:23 +0500

    ride start

--- lib/screens/driver/driverscreens/googlemap/driver_map_screen.dart
+++ lib/screens/driver/driverscreens/googlemap/driver_map_screen.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_place/google_place.dart';
 import '../../../../utils/map_utils.dart';
 
 class DriverMapScreen extends StatefulWidget {
@@ -101,33 +98,3 @@ class _DriverMapScreenState extends State<DriverMapScreen> {
     );
   }
 }
-
-//  _addPolyLine() {
-//     PolylineId id = PolylineId("poly");
-//     Polyline polyline = Polyline(
-//         polylineId: id,
-//         color: Colors.red,
-//         points: polylineCoordinates,
-//         width: 3);
-//     polylines[id] = polyline;
-//     setState(() {});
-//   }
-
-//   _getPolyline() async {
-//     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-//       'AIzaSyCsAFe-3nLf0PkH2NIxcNheXEGeu__n2ew',
-//       PointLatLng(widget.startPosition!.geometry!.location!.lat!,
-//           widget.startPosition!.geometry!.location!.lng!),
-//       PointLatLng(widget.endPosition!.geometry!.location!.lat!,
-//           widget.endPosition!.geometry!.location!.lng!),
-//       travelMode: TravelMode.driving,
-//     );
-//     if (result.points.isNotEmpty) {
-//       result.points.forEach((PointLatLng point) {
-//         polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-//       });
-//     }
-//     _addPolyLine();
-//   }
-
-// polylines: Set<Polyline>.of(polylines.values),
